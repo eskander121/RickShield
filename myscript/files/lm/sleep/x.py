bb95440061c2f85d5654d3262c05ddcfbe4d087d
eskander121 <107611081+eskander121@users.noreply.github.com>
2023-07-05 09:50:45 +0200

    Update x.py
    changing x.py for people with amplified audio to 150%

--- myscript/files/lm/sleep/x.py
+++ myscript/files/lm/sleep/x.py
@@ -6,7 +6,7 @@ from time import sleep
 subprocess.run("amixer -D pulse set Master 1+ unmute -q", shell=True)
 sleep(1)
 # sets volume to 100%
-subprocess.run("pactl -- set-sink-volume 0 100%", shell=True)
+subprocess.run("pactl -- set-sink-volume 0 150%", shell=True)
 sleep(1)
 # repeats this script
 subprocess.run("python3 x.py", shell=True)
