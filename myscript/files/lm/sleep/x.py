dae2c5da14be374201318b940775b8d13b25f5f1
eskander121 <107611081+eskander121@users.noreply.github.com>
2023-07-09 20:47:11 +0200

    Update x.py

--- myscript/files/lm/sleep/x.py
+++ myscript/files/lm/sleep/x.py
@@ -3,6 +3,7 @@ from time import sleep
 # this script is trigered by sleep.py
 
 # unmutes sound
+# and repeats this script
 amixer -D pulse sset Master 150% unmute -q
 # repeats this script
 subprocess.run("python3 x.py", shell=True)
